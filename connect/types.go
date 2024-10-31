eb2229db624159d3c3c240b44baafeaa583ed6fe
Bruno Scheufler <git@brunoscheufler.com>
2024-10-31 10:40:05 +0100

    Update types.go

--- connect/types.go
+++ connect/types.go
@@ -55,9 +55,9 @@ type GatewayMessageTypeSyncData struct {
 const GatewayMessageTypeExecutorRequest GatewayMessageType = "executor-request"
 
 type GatewayMessageTypeExecutorRequestData struct {
-	RequestId string `json:"replyId"`
+	RequestId string `json:"request_id"`
 
-	AppId uuid.UUID `json:"appId"`
+	AppId uuid.UUID `json:"app_id"`
 
 	FunctionSlug string  `json:"fn_slug"`
 	StepId       *string `json:"step_id"`
