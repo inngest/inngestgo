13b2210d4615bc132dd13b9f9d0844e2830ad32c
Aaron Harper <aaron.harper@outlook.com>
2025-02-14 14:43:00 -0500

    Fix returning step.Invoke error doesn't skip retries (#100)

--- tests/invoke_test.go
+++ tests/invoke_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"context"
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -239,5 +240,165 @@ func TestInvoke(t *testing.T) {
 
 		r.Nil(invokeResult)
 		r.Equal("could not find function with ID: some-non-existent-fn", invokeErr.Error())
+	})
+
+	t.Run("swallowed error does cause invoke retry", func(t *testing.T) {
+		// If we swallow the step.Invoke error and continue, we do not retry the
+		// invoke.
+
+		ctx := context.Background()
+		r := require.New(t)
+		appName := randomSuffix("my-app")
+		h := inngestgo.NewHandler(appName, inngestgo.HandlerOpts{})
+
+		var childCounter int32
+		childFnName := "my-child-fn"
+		childFn := inngestgo.CreateFunction(
+			inngestgo.FunctionOpts{
+				ID:      childFnName,
+				Name:    childFnName,
+				Retries: inngestgo.IntPtr(0),
+			},
+			inngestgo.EventTrigger("never", nil),
+			func(
+				ctx context.Context,
+				input inngestgo.Input[any],
+			) (any, error) {
+				atomic.AddInt32(&childCounter, 1)
+				return nil, fmt.Errorf("oh no")
+			},
+		)
+
+		var runID string
+		var invokeErr error
+		eventName := randomSuffix("my-event")
+		parentFn := inngestgo.CreateFunction(
+			inngestgo.FunctionOpts{
+				ID:      "my-parent-fn",
+				Name:    "my-parent-fn",
+				Retries: inngestgo.IntPtr(0),
+			},
+			inngestgo.EventTrigger(eventName, nil),
+			func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+				runID = input.InputCtx.RunID
+				_, invokeErr = step.Invoke[any](ctx,
+					"invoke",
+					step.InvokeOpts{
+						FunctionId: fmt.Sprintf("%s-%s", appName, childFnName),
+					},
+				)
+
+				_, _ = step.Run(ctx, "a", func(ctx context.Context) (any, error) {
+					return nil, nil
+				})
+
+				return nil, nil
+			},
+		)
+
+		h.Register(childFn, parentFn)
+
+		server, sync := serve(t, h)
+		defer server.Close()
+		r.NoError(sync())
+
+		_, err := inngestgo.Send(ctx, inngestgo.Event{
+			Name: eventName,
+			Data: map[string]any{"foo": "bar"}},
+		)
+		r.NoError(err)
+
+		var run *Run
+		r.EventuallyWithT(func(ct *assert.CollectT) {
+			a := assert.New(ct)
+			run, err = getRun(runID)
+			if !a.NoError(err) {
+				return
+			}
+			a.Equal(enums.RunStatusCompleted.String(), run.Status)
+		}, 5*time.Second, time.Second)
+
+		r.Error(invokeErr)
+		r.Equal(int32(1), childCounter)
+	})
+
+	t.Run("failed invoke does not cause parent retry", func(t *testing.T) {
+		// Returning a step.Invoke error from a parent function does not cause a
+		// retry. Invoke retries are the responsibility of the invoked function.
+
+		ctx := context.Background()
+		r := require.New(t)
+		appName := randomSuffix("my-app")
+		h := inngestgo.NewHandler(appName, inngestgo.HandlerOpts{})
+
+		var childCounter int32
+		childFnName := "my-child-fn"
+		childFn := inngestgo.CreateFunction(
+			inngestgo.FunctionOpts{
+				ID:      childFnName,
+				Name:    childFnName,
+				Retries: inngestgo.IntPtr(0),
+			},
+			inngestgo.EventTrigger("never", nil),
+			func(
+				ctx context.Context,
+				input inngestgo.Input[any],
+			) (any, error) {
+				atomic.AddInt32(&childCounter, 1)
+				return nil, fmt.Errorf("oh no")
+			},
+		)
+
+		var runID string
+		var attempt int
+		eventName := randomSuffix("my-event")
+		parentFn := inngestgo.CreateFunction(
+			inngestgo.FunctionOpts{
+				ID:   "my-parent-fn",
+				Name: "my-parent-fn",
+
+				// Allow a retry because we need to assert that a retry does not
+				// happen.
+				Retries: inngestgo.IntPtr(1),
+			},
+			inngestgo.EventTrigger(eventName, nil),
+			func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+				runID = input.InputCtx.RunID
+				attempt = input.InputCtx.Attempt
+				return step.Invoke[any](ctx,
+					"invoke",
+					step.InvokeOpts{
+						FunctionId: fmt.Sprintf("%s-%s", appName, childFnName),
+					},
+				)
+			},
+		)
+
+		h.Register(childFn, parentFn)
+
+		server, sync := serve(t, h)
+		defer server.Close()
+		r.NoError(sync())
+
+		_, err := inngestgo.Send(ctx, inngestgo.Event{
+			Name: eventName,
+			Data: map[string]any{"foo": "bar"}},
+		)
+		r.NoError(err)
+
+		var run *Run
+		r.EventuallyWithT(func(ct *assert.CollectT) {
+			a := assert.New(ct)
+			run, err = getRun(runID)
+			if !a.NoError(err) {
+				return
+			}
+			a.Equal(enums.RunStatusFailed.String(), run.Status)
+		}, 5*time.Second, time.Second)
+
+		r.Equal(int32(1), childCounter)
+
+		// A retry never happened because we returned the step.Invoke error.
+		r.Equal(0, attempt)
 	})
 }
