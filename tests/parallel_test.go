07f58e23ff83d3238d302a323bf7879fc07e5485
Aaron Harper <aaron@inngest.com>
2025-03-14 16:31:37 -0400

    Make group.Parallel public

--- tests/parallel_test.go
+++ tests/parallel_test.go
@@ -7,24 +7,23 @@ import (
 	"time"
 
 	"github.com/inngest/inngestgo"
-	"github.com/inngest/inngestgo/experimental/group"
+	"github.com/inngest/inngestgo/group"
 	"github.com/inngest/inngestgo/step"
 	"github.com/stretchr/testify/require"
 )
 
 func TestParallel(t *testing.T) {
 	devEnv(t)
+
 	t.Run("successful with a mix of step kinds", func(t *testing.T) {
 		ctx := context.Background()
 		r := require.New(t)
 
-		state := struct {
-			invokedFnCounter int
-			step1ACounter    int
-			step1BCounter    int
-			stepAfterCounter int
-			parallelResults  []group.Result
-		}{}
+		var invokedFnCounter int
+		var step1ACounter int
+		var step1BCounter int
+		var stepAfterCounter int
+		var results group.Results
 
 		appName := randomSuffix("TestParallel")
 		c, err := inngestgo.NewClient(inngestgo.ClientOpts{AppID: appName})
@@ -38,7 +37,7 @@ func TestParallel(t *testing.T) {
 			},
 			inngestgo.EventTrigger("dummy", nil),
 			func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
-				state.invokedFnCounter++
+				invokedFnCounter++
 				return "invoked output", nil
 			},
 		)
@@ -53,7 +52,7 @@ func TestParallel(t *testing.T) {
 			},
 			inngestgo.EventTrigger(eventName, nil),
 			func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
-				state.parallelResults = group.Parallel(
+				results = group.Parallel(
 					ctx,
 					func(ctx context.Context) (any, error) {
 						return step.Invoke[any](ctx, "invoke", step.InvokeOpts{
@@ -62,13 +61,13 @@ func TestParallel(t *testing.T) {
 					},
 					func(ctx context.Context) (any, error) {
 						return step.Run(ctx, "1a", func(ctx context.Context) (int, error) {
-							state.step1ACounter++
+							step1ACounter++
 							return 1, nil
 						})
 					},
 					func(ctx context.Context) (any, error) {
 						return step.Run(ctx, "1b", func(ctx context.Context) (int, error) {
-							state.step1BCounter++
+							step1BCounter++
 							return 2, nil
 						})
 					},
@@ -83,9 +82,13 @@ func TestParallel(t *testing.T) {
 						})
 					},
 				)
+				err := results.AnyError()
+				if err != nil {
+					return nil, err
+				}
 
-				_, err := step.Run(ctx, "after", func(ctx context.Context) (any, error) {
-					state.stepAfterCounter++
+				_, err = step.Run(ctx, "after", func(ctx context.Context) (any, error) {
+					stepAfterCounter++
 					return nil, nil
 				})
 				if err != nil {
@@ -101,19 +104,16 @@ func TestParallel(t *testing.T) {
 		defer server.Close()
 		r.NoError(sync())
 
-		_, err = c.Send(ctx, inngestgo.Event{
-			Name: eventName,
-			Data: map[string]any{"foo": "bar"}},
-		)
+		_, err = c.Send(ctx, inngestgo.Event{Name: eventName})
 		r.NoError(err)
 
 		r.Eventually(func() bool {
-			return state.stepAfterCounter == 1
+			return stepAfterCounter == 1
 		}, 5*time.Second, 10*time.Millisecond)
-		r.Equal(1, state.invokedFnCounter)
-		r.Equal(1, state.step1ACounter)
-		r.Equal(1, state.step1BCounter)
-		r.Equal(state.parallelResults, []group.Result{
+		r.Equal(1, invokedFnCounter)
+		r.Equal(1, step1ACounter)
+		r.Equal(1, step1BCounter)
+		r.Equal(results, group.Results{
 			{Value: "invoked output"},
 			{Value: 1},
 			{Value: 2},
@@ -131,6 +131,7 @@ func TestParallel(t *testing.T) {
 		r.NoError(err)
 
 		var runID string
+		var results group.Results
 		eventName := randomSuffix("my-event")
 		_, err = inngestgo.CreateFunction(
 			c,
@@ -143,7 +144,7 @@ func TestParallel(t *testing.T) {
 			func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
 				runID = input.InputCtx.RunID
 
-				group.Parallel(
+				results = group.Parallel(
 					ctx,
 					func(ctx context.Context) (any, error) {
 						return step.Run(ctx, "1a", func(ctx context.Context) (int, error) {
@@ -156,8 +157,7 @@ func TestParallel(t *testing.T) {
 						})
 					},
 				)
-
-				return nil, nil
+				return nil, results.AnyError()
 			},
 		)
 		r.NoError(err)
@@ -166,10 +166,7 @@ func TestParallel(t *testing.T) {
 		defer server.Close()
 		r.NoError(sync())
 
-		_, err = c.Send(ctx, inngestgo.Event{
-			Name: eventName,
-			Data: map[string]any{"foo": "bar"}},
-		)
+		_, err = c.Send(ctx, inngestgo.Event{Name: eventName})
 		r.NoError(err)
 
 		run, err := waitForRun(&runID, StatusFailed)
@@ -179,5 +176,90 @@ func TestParallel(t *testing.T) {
 		r.True(ok)
 
 		r.Contains(output["message"], "function panicked: oops")
+	})
+
+	t.Run("sequential steps in group", func(t *testing.T) {
+		ctx := context.Background()
+		r := require.New(t)
+
+		c, err := inngestgo.NewClient(inngestgo.ClientOpts{
+			AppID: randomSuffix("app"),
+		})
+		r.NoError(err)
+
+		// Use a channel to block step 2 until step 1b has finished.
+		ch1B := make(chan struct{})
+
+		var logs []string
+		var step1ACounter int
+		var step1BCounter int
+		var step2Counter int
+		var stepAfterCounter int
+		eventName := randomSuffix("event")
+		_, err = inngestgo.CreateFunction(
+			c,
+			inngestgo.FunctionOpts{
+				ID: "fn",
+			},
+			inngestgo.EventTrigger(eventName, nil),
+			func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+				results := group.Parallel(
+					ctx,
+					func(ctx context.Context) (any, error) {
+						_, err := step.Run(ctx, "1a", func(ctx context.Context) (any, error) {
+							logs = append(logs, "1a")
+							step1ACounter++
+							return nil, nil
+						})
+						if err != nil {
+							return nil, err
+						}
+
+						return step.Run(ctx, "1b", func(ctx context.Context) (any, error) {
+							logs = append(logs, "1b")
+							step1BCounter++
+							ch1B <- struct{}{}
+							return nil, nil
+						})
+					},
+					func(ctx context.Context) (any, error) {
+						return step.Run(ctx, "2", func(ctx context.Context) (any, error) {
+							// Wait for 1b to finish
+							<-ch1B
+
+							logs = append(logs, "2")
+							step2Counter++
+							return nil, nil
+						})
+					},
+				)
+				err := results.AnyError()
+				if err != nil {
+					return nil, err
+				}
+
+				return step.Run(ctx, "after", func(ctx context.Context) (any, error) {
+					logs = append(logs, "after")
+					stepAfterCounter++
+					return nil, nil
+				})
+			},
+		)
+		r.NoError(err)
+
+		server, sync := serve(t, c)
+		defer server.Close()
+		r.NoError(sync())
+
+		_, err = c.Send(ctx, inngestgo.Event{Name: eventName})
+		r.NoError(err)
+
+		r.Eventually(func() bool {
+			return stepAfterCounter == 1
+		}, 5*time.Second, 10*time.Millisecond)
+		r.Equal(1, step1ACounter)
+		r.Equal(1, step1BCounter)
+		r.Equal(1, step2Counter)
+		r.Equal(logs, []string{"1a", "1b", "2", "after"})
 	})
 }
