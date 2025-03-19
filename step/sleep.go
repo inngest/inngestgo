3da5fe602db9ae18cd66a077b4872b4b557d5bc6
Tony Holdstock-Brown <tonyhb@gmail.com>
2025-03-19 09:15:05 -0700

    Add step.sleepUntil, with type constraints to allow times and strings

--- step/sleep.go
+++ step/sleep.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/inngest/inngest/pkg/dateutil"
 	"github.com/inngest/inngest/pkg/enums"
 	"github.com/inngest/inngest/pkg/execution/state"
 	str2duration "github.com/xhit/go-str2duration/v2"
@@ -16,6 +17,40 @@ type SleepOpts struct {
 }
 
 func Sleep(ctx context.Context, id string, duration time.Duration) {
+	mgr := preflight(ctx)
+	op := mgr.NewOp(enums.OpcodeSleep, id, nil)
+	if _, ok := mgr.Step(ctx, op); ok {
+		// We've already slept.
+		return
+	}
+	mgr.AppendOp(state.GeneratorOpcode{
+		ID:   op.MustHash(),
+		Op:   enums.OpcodeSleep,
+		Name: id,
+		Opts: map[string]any{
+			"duration": str2duration.String(duration),
+		},
+	})
+	panic(ControlHijack{})
+}
+
+type SleepUntilParam interface {
+	time.Time | string
+}
+
+func SleepUntil[T SleepUntilParam](ctx context.Context, id string, until T) {
+	var duration time.Duration
+
+	switch v := any(until).(type) {
+	case string:
+		// Parse the time using built-in standards.
+		t, _ := dateutil.Parse(until)
+		// XXX: Handle error.
+		duration = time.Until(t)
+	case time.Time:
+		duration = time.Until(v)
+	}
+
 	mgr := preflight(ctx)
 	op := mgr.NewOp(enums.OpcodeSleep, id, nil)
 	if _, ok := mgr.Step(ctx, op); ok {
