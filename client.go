3194e4db355dff9f8c10fdd69024179c4e033df8
Tony HB <tonyhb@gmail.com>
2025-05-19 12:34:38 -0700

    add recover such that internal http panics never fail

--- client.go
+++ client.go
@@ -251,7 +251,13 @@ func (a apiClient) Send(ctx context.Context, e any) (string, error) {
 	return res[0], nil
 }
 
-func (a apiClient) SendMany(ctx context.Context, e []any) ([]string, error) {
+func (a apiClient) SendMany(ctx context.Context, e []any) (ids []string, err error) {
+	go func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("panic sending events: %v", r)
+		}
+	}()
+
 	for _, e := range e {
 		if v, ok := e.(validatable); ok {
 			if err := v.Validate(); err != nil {
