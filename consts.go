037a5eb854a37f40cff67900f24957e566a4e4db
Aaron Harper <aaron.harper@outlook.com>
2025-05-21 20:25:59 -0400

    Fix serve origin and path overrides (#150)

--- consts.go
+++ consts.go
@@ -1,9 +1,5 @@
 package inngestgo
 
-import (
-	"runtime/debug"
-)
-
 const (
 	SDKAuthor         = "inngest"
 	SDKLanguage       = "go"
@@ -22,32 +18,5 @@ const (
 )
 
 var (
-	SDKVersion = ""
+	SDKVersion = "0.11.3"
 )
-
-func init() {
-	readBuildInfo()
-}
-
-func readBuildInfo() {
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		return
-	}
-
-	/*
-		Find and set the SDK version.
-
-		When imported into another project, its value will be something like
-		"v0.7.5-0.20250305172920-ddde6dd6f565".
-
-		When run within this project, it'll be "(devel)".
-	*/
-	const modulePath = "github.com/inngest/inngestgo"
-	for _, dep := range info.Deps {
-		if dep.Path == modulePath && dep.Version != "" {
-			SDKVersion = dep.Version
-			break
-		}
-	}
-}
