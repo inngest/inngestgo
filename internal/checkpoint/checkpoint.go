df5395833dc996eadd4265c028825784849f59fe
Tony HB <tonyhb@gmail.com>
2025-11-02 20:25:14 -0800

    make checkpoint type public by default

--- internal/checkpoint/checkpoint.go
+++ internal/checkpoint/checkpoint.go
@@ -8,38 +8,10 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/inngest/inngestgo/internal/opcode"
+	check "github.com/inngest/inngestgo/pkg/checkpoint"
 )
 
-// Config specifies the configuration for checkpointing.
-//
-// The zero config is the safest checkpoitning configuration (and is the same as ConfigSage).
-// This checkpoints after every step.Run, ensuring that step data is saved as quickly as possible
-// so that data is not lost.
-//
-// Tweaking config parameters allows you to "batch" many steps into a single checkpoint.
-// If your server dies before the checkpoint completes, step data will be lost and steps
-// will rerun.
-//
-// You should ALWAYS start with the zero config and only tweak these parameters
-// to further improve latency if necessary.
-type Config struct {
-	// MaxSteps represents the maximum number of steps to execute before checkpointing.
-	//
-	// This must be higher than zero to enable batching of steps.
-	MaxSteps int
-
-	// MaxInterval represents the maximum time that we wait after a step runs before checkpointing.
-	MaxInterval time.Duration
-
-	// StopAfterSteps represents the maximum number of steps the function can execute before
-	// stopping execution and waiting for another re-entry.  This is useful for serverless functions,
-	// ensuring that we create a new request (and serverless function lifecycle) after a maximum number
-	// of requests.
-	//
-	// If zero, there are no limits on the number of steps that can be executed, and the SDK will execute
-	// step.run calls until an async step is reached.
-	// StopAfterSteps int
-}
+type Config = check.Config
 
 type Opts struct {
 	// RunID is the run ID being checkpointed.
--- pkg/checkpoint/checkpoint.go
+++ pkg/checkpoint/checkpoint.go
@@ -2,11 +2,38 @@ package checkpoint
 
 import (
 	"time"
-
-	"github.com/inngest/inngestgo/internal/checkpoint"
 )
 
-type Config = checkpoint.Config
+// Config specifies the configuration for checkpointing.
+//
+// The zero config is the safest checkpoitning configuration (and is the same as ConfigSage).
+// This checkpoints after every step.Run, ensuring that step data is saved as quickly as possible
+// so that data is not lost.
+//
+// Tweaking config parameters allows you to "batch" many steps into a single checkpoint.
+// If your server dies before the checkpoint completes, step data will be lost and steps
+// will rerun.
+//
+// You should ALWAYS start with the zero config and only tweak these parameters
+// to further improve latency if necessary.
+type Config struct {
+	// MaxSteps represents the maximum number of steps to execute before checkpointing.
+	//
+	// This must be higher than zero to enable batching of steps.
+	MaxSteps int
+
+	// MaxInterval represents the maximum time that we wait after a step runs before checkpointing.
+	MaxInterval time.Duration
+
+	// StopAfterSteps represents the maximum number of steps the function can execute before
+	// stopping execution and waiting for another re-entry.  This is useful for serverless functions,
+	// ensuring that we create a new request (and serverless function lifecycle) after a maximum number
+	// of requests.
+	//
+	// If zero, there are no limits on the number of steps that can be executed, and the SDK will execute
+	// step.run calls until an async step is reached.
+	// StopAfterSteps int
+}
 
 const (
 	// AllSteps attempts to checkpoint as many steps as possible.
